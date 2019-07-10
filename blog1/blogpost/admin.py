2f55c5b84ea3d2d94d53644eccaecc287cda8a2c
lixing <lixing027768@163.com>
2019-07-10 16:41:08 +0800

    配置文件＋基本视图实现＋管理员权限设置

--- blog1/blogpost/admin.py
+++ blog1/blogpost/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# python manage.py createsuperuser 创建管理员命令
+# adminname = admin
+# adminpassword = l123456789
--- blog1/blogpost/views.py
+++ blog1/blogpost/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render_to_response
+import datetime
 
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
 from .models import BlogPost
+# Create your views here.
 
 
 def archive(request):
+    now = datetime.datetime.now()
+    url = reverse("blogpost:archive")
     posts = BlogPost.objects.all()
+
     return render_to_response("archive.html", locals())
